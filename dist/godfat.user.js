4caee67af51c9342bc4a1898abc78abafc5e4e4e
Fábio Ruiz <mckfabio@gmail.com>
2026-05-06 21:17:48 -0300

    Rebuild

--- dist/godfat.user.js
+++ dist/godfat.user.js
@@ -2,11 +2,11 @@
 // @name        Battle Cats GodFat QoL Tools (Fork)
 // @description Injects a lot of useful information to bc.godfat.org seed tracker
 // @namespace   https://github.com/fabiorzfreitas/bc-godfat-qol
-// @version     2.12.0
+// @version     2.12.1
 // @match       https://bc.godfat.org/*
 // @author      fabiorzfreitas
-// @updateURL   https://raw.githubusercontent.com/fabiorzfreitas/bc-godfat-qol/master/dist/godfat.user.js
-// @downloadURL https://raw.githubusercontent.com/fabiorzfreitas/bc-godfat-qol/master/dist/godfat.user.js
+// @updateURL   https://github.com/fabiorzfreitas/bc-godfat-qol/raw/refs/heads/master/dist/godfat.user.js
+// @downloadURL https://github.com/fabiorzfreitas/bc-godfat-qol/raw/refs/heads/master/dist/godfat.user.js
 // ==/UserScript==
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
